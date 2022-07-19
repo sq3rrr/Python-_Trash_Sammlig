4ab857826693dc5994c04053b3f91c9044f3da58
sq3rrr <108004851+sq3rrr@users.noreply.github.com>
2022-07-19 19:54:13 +0200

    Create eisvodeerstenonsense.py
    Eis vode erste python spielrerie

---
+++ eisvodeerstenonsense.py
@@ -0,0 +1,32 @@
+print("Welcome player, to the lost forest\n************************************\n************************************\n************************************\n\n           ze lost forest\n\n*****************************************\n******************************************\n*********************************************")
+
+
+name = input("Give yourself a username\npleaz type below\n:_")
+
+
+
+print ("cool bozz, your username for this round is:_ " +"< "+ name +" >")
+
+print ("*************\n***********\n_0_      <------ " + name +"\n |\n LL\n*******************\n**********************\n*****************")
+
+
+
+game = input("*************\n***********\n_0_. . . ?? Where do I go?\n |\n LL\n*******************\n**********************\n*****************\nGo left or right?\ntype the answer below\n:_")
+
+while game == "right" or game == "Right":
+
+   game =      input("*************\n***********\n_0_\n |\n LL\n*******************\n**********************\n*****************\nGo left or right?\ntype the answer below\n:_")
+
+
+
+print ("***     *****\n****      *****\n***     *****\n_0_\n |\n LL\n\n***   ****\n****     *****\n****      *****\n      ^       _0_\n      |        |\n               LL\nwell done you are a free man/women")
+
+
+
+exit = input("press any key but E to exit the program")
+
+while exit == "E" or exit=="e":
+
+    exit = input ("press any key but E to exit the program\n:_")
+
+print("goodbye, thanks for playing\n     _0_\n      |               <----- Happy " + " " + name +"\n      LL")
